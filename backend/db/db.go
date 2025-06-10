403cd3234e7447c7eb4b0484e6ecf919ce51a599
Kotaro Kanke <kanke.kooooo01@gmail.com>
2025-06-10 13:24:55 +0900

    dbの通信方式変更

--- backend/db/db.go
+++ backend/db/db.go
@@ -13,14 +13,17 @@ var DB *sql.DB
 
 func InitDB() error {
 	log.Println("[InitDB] 呼ばれた！")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
+	// host := os.Getenv("DB_HOST")
+	// port := os.Getenv("DB_PORT")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
+	connName := os.Getenv("INSTANCE_CONNECTION_NAME")
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	dsn := fmt.Sprintf(
+		"user=%s password=%s dbname=%s host=/cloudsql/%s sslmode=disable",
+		user, password, dbname, connName,
+	)
 
 	var err error
 	DB, err = sql.Open("postgres", dsn)
